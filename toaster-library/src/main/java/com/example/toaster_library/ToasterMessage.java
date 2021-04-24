04aab4af372d984dce8eaee3e84e8ddf84f4de96
Roman Matsa <matsars@gmail.com>
2021-04-24 09:11:21 +0300

    Initial commit

---
+++ toaster-library/src/main/java/com/example/toaster_library/ToasterMessage.java
@@ -0,0 +1,10 @@
+package com.example.toaster_library;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void show(Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
