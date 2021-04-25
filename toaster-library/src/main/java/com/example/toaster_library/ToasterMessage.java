bfbd20d73dd41830578528290610703da07a5105
Roman Matsa <matsars@gmail.com>
2021-04-25 16:42:11 +0300

    add log message

--- toaster-library/src/main/java/com/example/toaster_library/ToasterMessage.java
+++ toaster-library/src/main/java/com/example/toaster_library/ToasterMessage.java
@@ -1,10 +1,13 @@
 package com.example.toaster_library;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 public class ToasterMessage {
     public static void show(Context context, String message){
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+
+        Log.i("TAG:", "info.");
     }
 }
